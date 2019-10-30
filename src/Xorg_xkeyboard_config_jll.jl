29575d7ddad9dfe7bb034ffcd03ec84459a7415c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 17:57:08 +0000

    Snapshot of branch master taken on 2019-10-30T13:57:07.394-04:00 from "https://github.com/UnofficialJuliaMirror/Xorg_xkeyboard_config_jll.jl-33bec58e-1273-512f-9401-5d533626f822"

---
+++ src/Xorg_xkeyboard_config_jll.jl
@@ -0,0 +1,31 @@
+module Xorg_xkeyboard_config_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("33bec58e-1273-512f-9401-5d533626f822"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_xkeyboard_config", artifacts_toml) for e in artifacts["Xorg_xkeyboard_config"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Xorg_xkeyboard_config; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Xorg_xkeyboard_config_jll
